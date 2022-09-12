28a46851f331681a5414d2271df26ba018246059
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-12 01:00:55 +0100

    combinations two two

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,15 +10,15 @@ int main(void)
 
 	for (a = 0; a <= 99; a++)
 	{
-	for (b = 0; b <= 99; b++)
+	for (b = 1; b <= 99; b++)
 	{
 	if (a != b)
 	{
-	putchar(a / 10 + '0');
-	putchar(a % 10 + '0');
+	putchar((a / 10) + '0');
+	putchar((a % 10) + '0');
 	putchar(' ');
-	putchar(b / 10 + '0');
-	putchar(b % 10 + '0');
+	putchar((b / 10) + '0');
+	putchar((b % 10) + '0');
 	if (a != 98 || b != 99)
 	{
 	putchar(',');
