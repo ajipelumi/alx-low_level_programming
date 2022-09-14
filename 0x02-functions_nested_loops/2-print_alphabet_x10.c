aedbe86791397af7ad5a058786ed00f71c5e4fb0
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-14 13:46:27 +0100

    print alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+i#include "main.h"
+
+/**
+* print_alphabet_x10 - Prints a to z ten times followed by a new line
+*
+* Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int line, alpha;
+
+	for (line = 0; line <= 9; line++)
+	{
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
