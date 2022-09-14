4b0deda1878793ee53de5896fa02bc689f03865b
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-14 17:06:34 +0100

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 24; a++)
+	for (a = 0; a <= 24; a = a + 1)
 	{
-	for (b = 0; b <= 59; b++)
+	for (b = 0; b <= 59; b = b + 1)
 	{
 	_putchar((a / 10) + '0');
 	_putchar((a % 10) + '0');
