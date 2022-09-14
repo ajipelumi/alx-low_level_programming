c9e804f5ab45ddef2e76cb2fc467a0174dfb3243
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-14 03:14:52 +0100

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar(a..z
+	_putchar('\n');
+	return (0);
+}
