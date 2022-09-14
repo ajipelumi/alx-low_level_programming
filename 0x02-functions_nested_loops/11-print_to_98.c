ee485b6cbcd39d1ee531f8f97fbb17acaf20d796
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-15 00:47:00 +0100

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,23 +15,16 @@ void print_to_98(int n)
 	{
 	for (i = n; i > 98; i = i - 1)
 	{
-	_putchar(i);
-	_putchar(',');
-	_putchar(' ');
-	}
-	}
-	else if (n < 98)
-	{
-	for (i = n; i < 98; i = i + 1)
-	{
-	_putchar(i);
-	_putchar(',');
-	_putchar(' ');
+	printf("%d, ", i);
+	printf("%d\n", 98);
 	}
 	}
 	else
 	{
-	_putchar(i);
+	for (i = n; i < 98; i = i + 1)
+	{
+	printf("%d, ", i);
+	printf("%d\n", 98);
 	}
-	_putchar('\n');
+	}
 }
