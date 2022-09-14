c7d11a3729b7f2c3686d27c0923fd9a9e66e9933
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-14 23:05:48 +0100

    add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add -  adds two integers and returns the result
+ *
+ * @num1: first number input
+ * @num2: second number input
+ *
+ * Return: 0
+ */
+int add(int num1, int num2)
+{
+	int n;
+
+	n = num1 + num2;
+	_putchar(n);
+	return (n);
+}
