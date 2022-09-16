bf8e2f10b086d287448fd7c7f02ac4327e331b99
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-16 01:05:56 +0100

    print then ignore 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9 followed by \n
+ * print_most_numbers - prints the numbers, from 0 to 9 followed by \n
  *
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
