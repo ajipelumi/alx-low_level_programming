002e46ae6422429a526a2f4f08e7e89cc866945b
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-16 21:09:08 +0100

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,19 +17,23 @@ void fizz_buzz(void)
 	{
 	if (num % 3 == 0 && num % 5 == 0)
 	{
-	printf("FizzBuzz ");
+	printf(" FizzBuzz");
 	}
 	else if (num % 3 == 0)
 	{
-	printf("Fizz ");
+	printf(" Fizz");
 	}
 	else if (num % 5 == 0)
 	{
-	printf("Buzz ");
+	printf(" Buzz");
+	}
+	else if (num == 1)
+	{
+	printf("%d", num);
 	}
 	else
 	{
-	printf("%d ", num);
+	printf(" %d", num);
 	}
 	}
 	printf("\n");
