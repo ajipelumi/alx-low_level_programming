02e2f1cd4412f64dc84062e4147277686213da62
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-16 02:04:17 +0100

    print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,19 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i <= 10; i++)
 	{
 	for (j = 0; j <= 14; j++)
 	{
-	if (j <= 9)
+	k = j;
+	if (j > 9)
 	{
-	_putchar(j / 10 + '0');
+	_putchar(1 + 48);
+	k = j % 10;
 	}
-	_putchar(j % 10 + '0');
+	_putchar(k + 48);
 	}
 	_putchar('\n');
 	}
