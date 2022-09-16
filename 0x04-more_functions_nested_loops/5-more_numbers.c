e63e2ff5d13246891d63218f74bcff76884ae9de
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-16 01:28:44 +0100

    print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more numbers - prints 10 times the numbers, from 0 to 14 followed by \n
+ * more_numbers - prints 10 times the numbers, from 0 to 14 followed by \n
  *
  * Return: void
  */
