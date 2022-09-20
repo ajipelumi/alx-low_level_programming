5e90a8eff8eb442d63f2cb10795fa3782c204e6f
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-20 02:19:18 +0100

    readme and task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the pointer it points to
+ *
+ * @n: function input
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
