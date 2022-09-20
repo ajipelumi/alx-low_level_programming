7af17d7c443cd11a7d7d933e17e44b60a0120605
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-20 04:17:28 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts -  prints a string, followed by a new line, to stdout
+ *
+ * @str: function input
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
