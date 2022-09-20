0fdaa0dc8b3c0d3208ea9e7ad233850b32f8cb45
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-20 04:43:38 +0100

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ *
+ * @s: function input
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; s++)
+	{
+
+	}
+	for (--i; i >= 0; --i)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\n');
+}
