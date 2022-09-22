3e727dff5a12f5d9ed1c9240a6c0447dc880975d
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-22 20:08:42 +0100

    string comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp -  compares two strings.
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if equal, >0 if s1 > s2, <0 if s1 < s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result = strcmp(s1, s2);
+
+	return (result);
+}
