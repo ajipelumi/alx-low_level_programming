f2467c17314c65601658235bee54d5ed898466b7
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-22 22:41:50 +0100

    cap strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @str: string character
+ *
+ * Return: words in uppercase
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	if (str[i] >= 97 && str[i] <= 122)
+	{
+	if (i == 0)
+	{
+	str[i] -= 32;
+	}
+	if (str[i - 1] == 32 || str[i - 1] == 9 || str[i - 1] == 10 ||
+	str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46 ||
+	str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 ||
+	str[i - 1] == 40 || str[i - 1] == 41 || str[i - 1] == 123 ||
+	str[i - 1] == 124)
+	{
+	str[i] -= 32;
+	}
+	}
+	i++;
+	}
+	return (str);
+}
