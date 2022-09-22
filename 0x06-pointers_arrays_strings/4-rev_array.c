ccd60ada665a4d4097eff3d188f15baa7b271f3d
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-22 21:48:29 +0100

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,15 +12,14 @@
 
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, j = n;
+	int swap;
 
-	for (i = (n - 1); i >= 0; i--)
+	for (i = 0; i < j / 2; i++)
 	{
-	printf("%d", a[i]);
-	if (i != 0)
-	{
-	printf(", ");
+		swap = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = swap;
+		n--;
 	}
-	}
-	printf("\n");
 }
