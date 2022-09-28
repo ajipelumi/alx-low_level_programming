65e3f791a203e0b1475dbe971751c7ead63cb2db
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-28 20:13:32 +0100

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ *
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
