a3ef16b869e3b4da19fd0f8dbd0251ca5f0122cf
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-09-30 20:21:41 +0100

    number of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,6 +11,12 @@
 
 int main(int argc, char __attribute__((unused))*argv[])
 {
-	printf("%d\n", argc);
+	int i, n;
+
+	for (i = 0; i < argc; i++)
+	{
+	n = i;
+	}
+	printf("%d\n", n);
 	return (0);
 }
