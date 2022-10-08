4ba0b24d439dc2b186244e1a32f6b6cace797f15
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-10-09 00:16:50 +0100

    first 98 fibonacci sequence

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonacci numbers, starting
+ * with 1 and 2, followed by a new line.
+ *
+ * @void: no input
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long fb1 = 0, fb2 = 1;
+	unsigned long sum;
+
+	for (i = 0; i < 98; i++)
+	{
+	sum = fb1 + fb2;
+	printf("%lu", sum);
+	fb1 = fb2;
+	fb2 = sum;
+	if (i != 97)
+	{
+	printf(", ");
+	}
+	}
+	printf("\n");
+	return (0);
+}
