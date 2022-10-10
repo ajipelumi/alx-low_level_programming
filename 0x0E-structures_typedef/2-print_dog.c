36b7b71eaf3560da617c8a26d700b005c1e06902
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-10-11 00:19:34 +0100

    print struct type variable

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ *
+ * @d: pointer to struct variable
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL || d->age == 0 || d->owner == NULL)
+	{
+	printf("nil");
+	}
+	else if (d != NULL)
+	{
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+	}
+}
