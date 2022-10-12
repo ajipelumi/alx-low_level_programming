8917e7bfe5c936fcfd35a612e9473a0238ecf5f1
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-10-12 20:47:58 +0100

    integer index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,12 +17,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
+	if (size > 0 && array && cmp)
 	{
-	if (size <= 0)
-	{
-	return (-1);
-	}
 	for (i = 0; i < size; i++)
 	{
 	if (cmp(array[i]) != 0)
@@ -35,5 +31,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 	return (-1);
 	}
 	}
-	return (0);
+	return (-1);
 }
