1409c73379b087b907b09a44a6e8060fcfce74ec
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-10-13 15:40:15 +0100

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,9 +15,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-
-	if (n != 0)
-	{
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
@@ -30,7 +27,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 	}
 	}
-	printf("\n");
-	}
 	va_end(ap);
+	printf("\n");
 }
