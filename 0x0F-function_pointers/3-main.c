84ecc618b4ca373ec9dbc2f2de1e8f296494bea8
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-10-13 03:34:00 +0100

    calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ *
+ * @argc: argument counts
+ * @argv: argument vector
+ *
+ * Return: result of the operation
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	char *operator;
+	int result;
+	int (*ptr)(int, int);
+
+	if (argc != 4)
+	{
+	printf("Error\n");
+	exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	{
+	printf("Error\n");
+	exit(99);
+	}
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
+	{
+	exit(100);
+	}
+	ptr = get_op_func(operator);
+	result = ptr(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+/**
+ * op_add - sum of a and b
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of the two numbers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of a and b
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of the two numbers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of a and b
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: product of the two numbers
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides a by b
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of the division of the two numbers
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus of a and b
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder of the division of the two numbers
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
