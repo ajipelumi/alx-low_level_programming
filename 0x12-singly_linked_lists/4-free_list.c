cf6dc9eeb2bc51f1976cad4d4ff30e5d4751a2b6
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-10-21 04:49:41 +0100

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ *
+ * @head: pointer to the nodes
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
+}
