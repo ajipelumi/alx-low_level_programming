80faebb80e2723878958d3346c2dd21f4e025090
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-10-25 23:07:04 +0100

    free list and set to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ *
+ * @head: pointer to the nodes to free
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *new_temp;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		new_temp = temp->next;
+		free(temp);
+		temp = new_temp;
+	}
+	*head = NULL;
+}
