ada3f8eac16ff5e4c4ea64308348685a0c67d76d
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-10-26 19:58:06 +0100

    insert a node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -50,5 +50,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = NULL; /* next address bcomes NULL */
 		temp->next = new; /* link previous node to new node */
 	}
-	return (*head);
+	return (new);
 }
