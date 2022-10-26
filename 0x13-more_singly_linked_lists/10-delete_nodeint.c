aede9a99a6f160c686c0c53c8c28faa4325eca11
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-10-26 22:44:42 +0100

    delete node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -35,7 +35,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	for (i = 0; i < (index - 1) && *head; i++)
 	{
 		temp = temp->next;
-		if (!temp || !temp->next)
+		if (!temp || !(temp->next))
 		{
 			return (-1);
 		}
