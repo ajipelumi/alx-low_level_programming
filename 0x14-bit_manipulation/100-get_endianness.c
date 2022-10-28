706013040ec89f50fb16e02fe01191086fb98224
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-10-28 18:29:53 +0100

    check endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c;
+
+	c = (char *) &i; /* points to the first byte of the integer i */
+	if (*c == 1)
+	{
+		return (1);
+	}
+	return (0);
+}
