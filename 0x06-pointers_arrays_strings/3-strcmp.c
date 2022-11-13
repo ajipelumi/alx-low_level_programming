8601e46a9f2699a73f945e4d97eb193644b8308c
Ajisafe Oluwapelumi <106672692+ajipelumi@users.noreply.github.com>
2022-11-13 23:19:53 +0100

    function update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,24 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int result = strcmp(s1, s2);
+	int i = 0;
+	int flag = 0;
 
-	return (result);
+	while (flag == 0)
+	{
+		if (s1[i] > s2[i])
+		{
+			flag = 1;	
+		}
+		else if (s1[i] < s2[i])
+		{
+			flag = -1;	
+		}
+		if (s1[i] == '\0')
+		{
+			break;	
+		}
+		i++;
+	}
+	return (flag);
 }
