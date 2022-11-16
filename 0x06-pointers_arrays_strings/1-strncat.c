f4c1f96b3c51de0e2b478d96353e844d25c92d67
Ajisafe Oluwapelumi <106672692+ajipelumi@users.noreply.github.com>
2022-11-16 01:18:50 +0100

    strncat update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,6 +14,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int i;
+	int len;
+	
+	for (len = 0; dest[len] != '\0'; len++)
+	{
+	
+	}
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+	dest[len] = '\0';
 	return (dest);
 }
