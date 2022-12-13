e5c06dd32f30138acce1aa8a648b43d15fefd66c
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-12-13 19:01:25 +0100

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new;
 		return (*h);
 	}
-	if ((*h)->prev == NULL) /* first index */
+	if (idx == 0 || (*h)->prev == NULL) /* first index */
 	{
 		(*h)->prev = new;
 		new->next = *h;
