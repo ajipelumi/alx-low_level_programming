3ec6055fa0d632d9af56a87992c94817d380707c
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-06 14:25:45 +0100

    create table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -23,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		}
 
 		table->size = size;
-		table->array = calloc(table->size, sizeof(hash_node_t*));
+		table->array = calloc(table->size, sizeof(hash_node_t *));
 		if (table->array == NULL) /* calloc fails */
 		{
 			return (NULL);
