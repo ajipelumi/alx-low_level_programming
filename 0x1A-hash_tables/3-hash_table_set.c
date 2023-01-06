e57522147e80b9091f8d7e01bafc8ccb1ddd6032
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-06 22:41:38 +0100

    add

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,11 +24,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	if (strlen(key) == 0) /* empty key */
-	{
-		return (0);
-	}
-
 	hash = hash_djb2((const unsigned char *)key); /* generate hash value */
 	index = hash % ht->size; /* retrieve index */
 
