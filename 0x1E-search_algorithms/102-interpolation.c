464a4e8d5d0f409cb01f8518669950daba955daa
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-04-12 19:01:02 +0100

    clean up

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -19,9 +19,11 @@ int interpolation_search(int *array, size_t size, int value)
 
 	low = 0;
 	high = size - 1;
-	while (array[high] != array[low] && value >= array[low] && value <= array[high])
+	while (array[high] != array[low] && (value >= array[low])
+			&& (value <= array[high]))
 	{
-		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		pos = low + (((double)(high - low) / (array[high] - array[low])) *
+		(value - array[low]));
 		printf("Value checked array[%d] = [%d]\n", (int)pos, array[pos]);
 		if (array[pos] < value)
 		{
